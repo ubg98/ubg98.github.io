0defffcd7723d98a70407a2c2ddad8a5eb74eafa
games235 <games235@skm.vn>
2024-01-29 15:56:49 +0700

    Update

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const fs = require('fs');
 async function makeIndex() {
   const c6xPath= "./@orginal/launch";
   const FILEs= fs.readdirSync(c6xPath);
+  let totalGame= 0;
+  let totalFrame= 0;
   indexHTML= "<html><head><title>Index of /classroom6x</title>\n";
   indexHTML+= "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, shrink-to-fit=no\">\n";
-  indexHTML+= "</head><body><h1>Index of /classroom6x</h1><hr>";
+  indexHTML+= "</head><body><h1>Index of /classroom6x</h1><hr></hr>";
   FILEs.forEach(async (file)=> {
     // console.log("file", file);
     if (file.indexOf(".html")>= 0) {
+      totalGame++;
       const content= fs.readFileSync(c6xPath+ "/"+ file).toString();
       const frame= content.split("<iframe")?.[1].split("src=\"")?.[1].split("\"")[0]?? "";
       const game= file.split(".html")[0];
@@ -20,6 +23,7 @@ async function makeIndex() {
       } catch (e) {}
       indexHTML+= `<a href="/${game}/">/${game}/</a><br>`;
       if (frame.indexOf("/classroom6x.gitlab.io/")<= -1)  {
+        totalFrame++;
         indexFrame= `<html><body style="padding:0px;margin:0px;"><iframe src="${frame}" width= "100%" height= "100%" frameborder="0"></iframe></body></html>`;
         try {
           fs.mkdirSync(game);
@@ -28,6 +32,7 @@ async function makeIndex() {
       }
     }
   });
+  indexHTML+= `<hr></hr><h4>Games: ${totalGame}, Frames: ${totalFrame}</h4>`;
   fs.writeFileSync("index.html", indexHTML);
 }
 
