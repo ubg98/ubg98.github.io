48e2fd1ccca3c41f2e33e5f36525d36ec383fb22
games235 <games235@skm.vn>
2024-01-30 16:33:07 +0700

    Update

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ async function makeIndex() {
   indexHTML= "<html><head><title>Index of /classroom6x</title>\n";
   indexHTML+= "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, shrink-to-fit=no\">\n";
   indexHTML+= "</head><body><h1>Index of /classroom6x</h1><hr></hr>";
+  mapXML= "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+  mapXML+= "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" ";
+  mapXML+= "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" ";
+  mapXML+= "xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">\n";
   FILEs.forEach(async (file)=> {
     // console.log("file", file);
     if (file.indexOf(".html")>= 0) {
@@ -22,6 +26,7 @@ async function makeIndex() {
         fs.rmdirSync(game);
       } catch (e) {}
       indexHTML+= `<a href="/${game}/">/${game}/</a><br>`;
+      mapXML+= `<url><loc>/${game}/</loc></url>\n`;
       if (frame.indexOf("/classroom6x.gitlab.io/")<= -1)  {
         totalFrame++;
         indexFrame= `<html><body style="padding:0px;margin:0px;"><iframe src="${frame}" width= "100%" height= "100%" frameborder="0"></iframe></body></html>`;
@@ -33,7 +38,9 @@ async function makeIndex() {
     }
   });
   indexHTML+= `<hr></hr><h4>Games: ${totalGame}, Frames: ${totalFrame}</h4>`;
+  mapXML+= "</urlset>";
   fs.writeFileSync("index.html", indexHTML);
+  fs.writeFileSync("sitemap.html", mapXML);
 }
 
 
